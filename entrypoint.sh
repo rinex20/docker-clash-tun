c36255ad53164c34792096a77d5d57641702b76a
rinex20 <rinex20@me.com>
2021-09-02 16:42:15 +0800

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,31 +2,32 @@
 
 /clash
 
-# TUN Interface
-TUN_ADDR='198.18.0.1'
-TUN_NET='198.18.0.0/16'
-TUN_DEV='utun'
+# Based on https://github.com/Kr328/kr328-clash-setup-scripts/blob/master/setup-clash-tun.sh
+ipset create localnetwork hash:net
+ipset add localnetwork 127.0.0.0/8
+ipset add localnetwork 10.0.0.0/8
+ipset add localnetwork 169.254.0.0/16
+ipset add localnetwork 192.168.0.0/16
+ipset add localnetwork 224.0.0.0/4
+ipset add localnetwork 240.0.0.0/4
+ipset add localnetwork 172.16.0.0/12
 
-# MISC
-CLASH_TABLE='0x164'
-CLASH_USER='root'
-CLASH_UID=$(id -u "$CLASH_USER")
-MAIN_TABLE='main'
+ip route replace default dev utun table 0x162
 
-SELF=$(readlink -f "$0")
-BASEDIR=$(dirname "$SELF")
+ip rule add fwmark 0x162 lookup 0x162
 
-#wait for TUN device
-while ! ip address show "$TUN_DEV" > /dev/null; do
-    sleep 0.2
-done
+iptables -t mangle -N CLASH
+iptables -t mangle -F CLASH
+iptables -t mangle -A CLASH -p tcp --dport 53 -j MARK --set-mark 0x162
+iptables -t mangle -A CLASH -p udp --dport 53 -j MARK --set-mark 0x162
+iptables -t mangle -A CLASH -m addrtype --dst-type BROADCAST -j RETURN
+iptables -t mangle -A CLASH -m set --match-set localnetwork dst -j RETURN
+iptables -t mangle -A CLASH -d 198.18.0.0/16 -j MARK --set-mark 0x162
+iptables -t mangle -A CLASH -j MARK --set-mark 0x162
 
-ip route flush table "$CLASH_TABLE"
-ip route add default via "$TUN_ADDR" dev "$TUN_DEV" table "$CLASH_TABLE"
-ip route add "$TUN_NET" dev "$TUN_DEV" table "$CLASH_TABLE"
+iptables -t mangle -I OUTPUT -j CLASH
+iptables -t mangle -I PREROUTING -m set ! --match-set localnetwork dst -j MARK --set-mark 0x162
 
-ip rule add from all table "$CLASH_TABLE"
-ip rule add uidrange "${CLASH_UID}-${CLASH_UID}" table "$MAIN_TABLE"
-ip rule add to 192.168.0.0/16 table "$MAIN_TABLE"
-ip rule add to 172.16.0.0/12 table "$MAIN_TABLE"
-ip rule add to 10.0.0.0/8 table "$MAIN_TABLE"
+
+sysctl -w net/ipv4/ip_forward=1
+sysctl -w net.ipv4.conf.utun0.rp_filter=0
